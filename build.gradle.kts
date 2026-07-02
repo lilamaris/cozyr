bce2c3019ae0ab8e78f046bd726e293279bfb3ee
lilamaris <lilamaris.dev@gmail.com>
2026-07-02 18:17:15 +0900

    build(deps): 멀티 모듈 빌드 구성

---
+++ build.gradle.kts
@@ -0,0 +1,2 @@
+group = "com.lilamaris.cozyr"
+version = "0.0.1-SNAPSHOT"
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        includeBuild("build-logic")
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    repositories {
+        mavenCentral()
+    }
+}
+
+rootProject.name = "cozyr"
