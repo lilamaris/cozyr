82c4f942bd573ed94c0392cf12fffa5ad7f7c8f8
lilamaris <lilamaris.dev@gmail.com>
2026-07-03 21:42:11 +0900

    feat(board-service): 게시판 도메인 추가

---
+++ board-service/domain/src/main/java/com/lilamaris/cozyr/board/domain/Board.java
@@ -0,0 +1,59 @@
+package com.lilamaris.cozyr.board.domain;
+
+import com.lilamaris.cozyr.kernel.core.condition.ObjectPrecondition;
+import com.lilamaris.cozyr.kernel.core.condition.StringPrecondition;
+import com.lilamaris.cozyr.kernel.core.condition.TimePrecondition;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "board")
+@Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "description", nullable = false, length = 300)
+    private String description;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    private Board(String name, String description, Instant createdAt, Instant updatedAt) {
+        this.name = StringPrecondition.requireNonBlank(name, "name");
+        this.description = ObjectPrecondition.requireNonNull(description, "description");
+        this.createdAt = ObjectPrecondition.requireNonNull(createdAt, "createdAt");
+        this.updatedAt = updatedAt;
+
+        if (updatedAt != null) {
+            TimePrecondition.requireAfterOrEqual(updatedAt, createdAt, "updatedAt", "createdAt");
+        }
+    }
+
+    public static Board of(String name, String description, Instant createdAt) {
+        return new Board(name, description, createdAt, null);
+    }
+
+    public void updateName(String name, Instant updatedAt) {
+        this.updatedAt = TimePrecondition.requireAfterOrEqual(updatedAt, createdAt, "updatedAt", "createdAt");
+        this.name = StringPrecondition.requireNonBlank(name, "name");
+    }
+
+    public void updateDescription(String description, Instant updatedAt) {
+        this.updatedAt = TimePrecondition.requireAfterOrEqual(updatedAt, createdAt, "updatedAt", "createdAt");
+        this.description = StringPrecondition.requireNonBlank(description, "description");
+    }
+}
