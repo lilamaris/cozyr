2bc5598e6a9fc3f954f8653787a41375c1fd323b
lilamaris <lilamaris.dev@gmail.com>
2026-07-15 09:10:07 +0900

    feat(identity-service): 인증 서비스 실행 모듈 추가

---
+++ identity-service/launcher/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("cozyr.spring.module")
+    id("cozyr.spring.observability")
+    id("cozyr.spring.autoconfigure")
+    alias(libs.plugins.spring.boot)
+}
+
+group = "com.lilamaris.cozyr"
+version = "0.0.1-SNAPSHOT"
+
+dependencies {
+    implementation(project(":identity-service:application"))
+    implementation(project(":identity-service:domain"))
+    implementation(project(":identity-service:jpa"))
+    implementation(project(":identity-service:fileIO"))
+    implementation(project(":identity-service:security"))
+    implementation(project(":identity-service:web"))
+}
---
+++ identity-service/launcher/src/main/java/com/lilamaris/cozyr/identity/IdentityServiceLauncher.java
@@ -0,0 +1,11 @@
+package com.lilamaris.cozyr.identity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityServiceLauncher {
+    public static void main(String[] args) {
+        SpringApplication.run(IdentityServiceLauncher.class);
+    }
+}
