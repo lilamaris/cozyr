29969fe12933890be59f34d58975f5866a9c5402
lilamaris <lilamaris.dev@gmail.com>
2026-07-15 09:09:45 +0900

    feat(identity-service): 인증 웹 모듈 추가

---
+++ identity-service/web/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("cozyr.spring.module")
+    id("cozyr.spring.web")
+}
+
+group = "com.lilamaris.cozyr"
+version = "0.0.1-SNAPSHOT"
+
+dependencies {
+    implementation(project(":identity-service:application"))
+}
