ce010ceb0cf494d4f466864138db21b770b46f8c
lilamaris <lilamaris.dev@gmail.com>
2026-07-30 05:58:03 +0900

    fix(board-service): opendoc 요청 접근 제어 설정 추가
    Signed-off-by: lilamaris <lilamaris.dev@gmail.com>

--- board-service/security/src/main/java/com/lilamaris/cozyr/board/security/config/SecurityConfiguration.java
+++ board-service/security/src/main/java/com/lilamaris/cozyr/board/security/config/SecurityConfiguration.java
@@ -21,6 +21,12 @@ public class SecurityConfiguration {
             IdentityAuthenticationConverter converter
     ) {
         http
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        http
                 .oauth2ResourceServer(
                         oauth2 -> oauth2.jwt(
                                 jwt -> jwt
