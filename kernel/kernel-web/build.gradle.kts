4f0d37d3bde1d81afbe873105cbdd008b56dcb79
lilamaris <lilamaris.dev@gmail.com>
2026-08-03 08:22:14 +0900

    build(kernel): web 모듈 의존성 정리
    Signed-off-by: lilamaris <lilamaris.dev@gmail.com>

--- kernel/kernel-web/build.gradle.kts
+++ kernel/kernel-web/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("cozyr.java.module")
-    id("cozyr.spring.web")
+    id("cozyr.spring.platform")
     `java-library`
 }
 
@@ -10,6 +10,9 @@ version = "0.0.1-SNAPSHOT"
 dependencies {
     api(project(":kernel:kernel-core"))
     api(project(":kernel:kernel-application"))
+    api(libs.spring.web)
+    api(libs.jakarta.servlet.api)
+    api(libs.tools.jackson.databind)
 
     testImplementation(project(":kernel:kernel-test"))
 }
