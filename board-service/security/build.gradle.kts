dc6a7157514472018d5c8d1732b7bbc5a41a88bf
lilamaris <lilamaris.dev@gmail.com>
2026-08-03 09:44:55 +0900

    feat(board-service): 커널 Security 예외 핸들러 적용
    Signed-off-by: lilamaris <lilamaris.dev@gmail.com>

--- board-service/security/build.gradle.kts
+++ board-service/security/build.gradle.kts
@@ -8,7 +8,7 @@ group = "com.lilamaris.cozyr"
 version = "0.0.1-SNAPSHOT"
 
 dependencies {
-    implementation(project(":kernel:kernel-core"))
+    implementation(project(":kernel:kernel-security"))
     implementation(project(":identity-service:identity-resource-server"))
 
     testImplementation(project(":kernel:kernel-test"))
--- board-service/security/src/main/java/com/lilamaris/cozyr/board/security/config/SecurityConfiguration.java
+++ board-service/security/src/main/java/com/lilamaris/cozyr/board/security/config/SecurityConfiguration.java
@@ -2,23 +2,64 @@ package com.lilamaris.cozyr.board.security.config;
 
 import com.lilamaris.cozyr.identity.resource.server.filter.IdentityAuthenticationConverter;
 import com.lilamaris.cozyr.identity.resource.server.filter.IdentityContextBindingFilter;
+import com.lilamaris.cozyr.kernel.security.handler.ProblemDetailAccessDeniedHandler;
+import com.lilamaris.cozyr.kernel.security.handler.ProblemDetailAuthenticationEntryPoint;
+import com.lilamaris.cozyr.kernel.web.response.ProblemDetailFactory;
+import com.lilamaris.cozyr.kernel.web.response.ServletJsonResponseWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import tools.jackson.databind.ObjectMapper;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
     @Bean
+    Customizer<OAuth2ResourceServerConfigurer<HttpSecurity>.JwtConfigurer> jwtConfigurerCustomizer(
+            JwtDecoder jwtDecoder,
+            IdentityAuthenticationConverter authenticationConverter
+    ) {
+        return jwt -> jwt
+                .decoder(jwtDecoder)
+                .jwtAuthenticationConverter(authenticationConverter);
+    }
+
+    @Bean
+    ProblemDetailAccessDeniedHandler problemDetailAccessDeniedHandler(
+            ProblemDetailFactory problemDetailFactory,
+            ServletJsonResponseWriter responseWriter
+    ) {
+        return new ProblemDetailAccessDeniedHandler(problemDetailFactory, responseWriter);
+    }
+
+    @Bean
+    ProblemDetailAuthenticationEntryPoint problemDetailAuthenticationEntryPoint(
+            ProblemDetailFactory problemDetailFactory,
+            ServletJsonResponseWriter responseWriter
+    ) {
+        return new ProblemDetailAuthenticationEntryPoint(problemDetailFactory, responseWriter);
+    }
+
+    @Bean
+    ServletJsonResponseWriter servletJsonResponseWriter(ObjectMapper objectMapper) {
+        return new ServletJsonResponseWriter(objectMapper);
+    }
+
+    @Bean
     SecurityFilterChain securityFilterChain(
             HttpSecurity http,
-            JwtDecoder jwtDecoder,
-            IdentityContextBindingFilter filter,
-            IdentityAuthenticationConverter converter
+            Customizer<OAuth2ResourceServerConfigurer<HttpSecurity>.JwtConfigurer> jwtConfigurerCustomizer,
+            AuthenticationEntryPoint authenticationEntryPoint,
+            AccessDeniedHandler accessDeniedHandler,
+            IdentityContextBindingFilter filter
     ) {
         http
                 .authorizeHttpRequests(request -> request
@@ -28,12 +69,11 @@ public class SecurityConfiguration {
 
         http
                 .oauth2ResourceServer(
-                        oauth2 -> oauth2.jwt(
-                                jwt -> jwt
-                                        .decoder(jwtDecoder)
-                                        .jwtAuthenticationConverter(converter)
-                        )
+                        oauth2 -> oauth2
+                                .jwt(jwtConfigurerCustomizer)
+                                .authenticationEntryPoint(authenticationEntryPoint)
                 )
+                .exceptionHandling(exception -> exception.accessDeniedHandler(accessDeniedHandler))
                 .addFilterAfter(filter, BearerTokenAuthenticationFilter.class);
 
         return http.build();
