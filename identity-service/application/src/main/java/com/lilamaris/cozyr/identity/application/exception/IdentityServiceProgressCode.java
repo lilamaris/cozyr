6e14548322522f601c0df17f97f7ca45a4054109
lilamaris <lilamaris.dev@gmail.com>
2026-08-03 09:44:55 +0900

    fix(identity-service): 인증 실패 애플리케이션 코드 타입 변경
    Signed-off-by: lilamaris <lilamaris.dev@gmail.com>

--- identity-service/application/src/main/java/com/lilamaris/cozyr/identity/application/exception/IdentityServiceProgressCode.java
+++ identity-service/application/src/main/java/com/lilamaris/cozyr/identity/application/exception/IdentityServiceProgressCode.java
@@ -9,7 +9,7 @@ public enum IdentityServiceProgressCode implements ApplicationProgressCode {
     USER_NOT_FOUND(ProcessReason.REJECTED, "user", "not-found", "사용자를 찾을 수 없습니다."),
     ACCOUNT_NOT_FOUND(ProcessReason.REJECTED, "account", "not-found", "계정을 찾을 수 없습니다."),
     EMAIL_DUPLICATED(ProcessReason.REJECTED, "account", "duplicated", "이미 사용 중인 이메일입니다."),
-    AUTHENTICATE_FAILED(ProcessReason.REJECTED, "account", "failed", "인증 실패.")
+    AUTHENTICATE_FAILED(ProcessReason.REJECTED, "account", "unauthorized", "인증 실패.")
     ;
 
     private final ProcessReason reason;
