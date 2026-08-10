f8d13f560d4b5485b46594a46aab4919973d3580
lilamaris <lilamaris.dev@gmail.com>
2026-08-11 00:13:26 +0900

    fix(docker): 배포용 도커 컴포즈 구성에 statistics-service 설정 추가
    Signed-off-by: lilamaris <lilamaris.dev@gmail.com>

--- docker/infra/conf/postgres/init/01-create-users-and-schemas.sh
+++ docker/infra/conf/postgres/init/01-create-users-and-schemas.sh
@@ -3,13 +3,15 @@ set -euo pipefail
 
 : "${IDENTITY_DB_PASSWORD:?IDENTITY_DB_PASSWORD is required}"
 : "${BOARD_DB_PASSWORD:?BOARD_DB_PASSWORD is required}"
+: "${STATISTICS_DB_PASSWORD:?STATISTICS_DB_PASSWORD is required}"
 
 psql \
   --username "$POSTGRES_USER" \
   --dbname "$POSTGRES_DB" \
   --set=db_name="$POSTGRES_DB" \
   --set=identity_password="$IDENTITY_DB_PASSWORD" \
-  --set=board_password="$BOARD_DB_PASSWORD" <<'SQL'
+  --set=board_password="$BOARD_DB_PASSWORD" \
+  --set=statistics_password="$STATISTICS_DB_PASSWORD" <<'SQL'
 CREATE ROLE identity_user
     LOGIN
     PASSWORD :'identity_password'
@@ -24,8 +26,16 @@ CREATE ROLE board_user
     NOCREATEDB
     NOCREATEROLE;
 
+CREATE ROLE statistics_user
+  LOGIN
+  PASSWORD :'statistics_password'
+  NOSUPERUSER
+  NOCREATEDB
+  NOCREATEROLE;
+
 CREATE SCHEMA identity AUTHORIZATION identity_user;
 CREATE SCHEMA board AUTHORIZATION board_user;
+CREATE SCHEMA statistics AUTHORIZATION statistics_user;
 
 ALTER ROLE identity_user IN DATABASE :"db_name"
     SET search_path = identity, pg_catalog;
@@ -33,7 +43,11 @@ ALTER ROLE identity_user IN DATABASE :"db_name"
 ALTER ROLE board_user IN DATABASE :"db_name"
     SET search_path = board, pg_catalog;
 
+ALTER ROLE statistics_user IN DATABASE :"db_name"
+    SET search_path = statistics, pg_catalog;
+
 REVOKE CREATE ON SCHEMA public FROM PUBLIC;
 REVOKE ALL ON SCHEMA identity FROM PUBLIC;
 REVOKE ALL ON SCHEMA board FROM PUBLIC;
+REVOKE ALL ON SCHEMA statistics FROM PUBLIC;
 SQL
